dfd1926076eaa4a37c506356a4e84da6cc6f8f7b
Mark <sipizork@gmail.com>
2021-01-16 11:19:59 +0100

    first commit

---
+++ clock.js
@@ -0,0 +1,13 @@
+const second = document.querySelector('.second');
+const minute = document.querySelector('.minute');
+const hour = document.querySelector('.hour');
+
+setInterval(() => {
+  let date = new Date();
+  second_deg = date.getSeconds() * 6 + 180;
+  minute_deg = (date.getMinutes() * 6) + (date.getSeconds() / 10) + 180;
+  hour_deg = (date.getHours() * 30) + (date.getMinutes() / 2) + (date.getSeconds() * 0.0083) + 180;
+  minute.style.transform = `rotate(${minute_deg}deg)`;
+  second.style.transform = `rotate(${second_deg}deg)`;
+  hour.style.transform = `rotate(${hour_deg}deg)`;
+}, 1000);
